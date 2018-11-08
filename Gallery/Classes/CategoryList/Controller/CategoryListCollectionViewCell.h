4577bbe7b7fca7d69d715f35f6ed2ace4c485f97
andong <dongan708@gmail.com>
2018-11-08 16:01:23 +0800

    updates

---
+++ Gallery/Classes/CategoryList/Controller/CategoryListCollectionViewCell.h
@@ -0,0 +1,18 @@
+//
+//  CategoryListCollectionViewCell.h
+//  Gallery
+//
+//  Created by admin on 2018/11/8.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryListCollectionViewCell : UICollectionViewCell
+
+//什么鬼
+@end
+
+NS_ASSUME_NONNULL_END
