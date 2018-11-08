f67201b010833a2b5ced44d3ec537e3fbdd90ffd
andong <dongan708@gmail.com>
2018-11-08 18:36:35 +0800

    updates

--- Gallery/Classes/Home/Controller/HomeViewController.m
+++ Gallery/Classes/Home/Controller/HomeViewController.m
@@ -10,6 +10,7 @@
 
 @interface HomeViewController ()
 
+
 @end
 
 @implementation HomeViewController
@@ -20,14 +21,5 @@
   self.navigationItem.title = @"首页";
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Podfile
+++ Podfile
@@ -9,5 +9,6 @@ pod 'Masonry',            '1.1.0', :inhibit_warnings => true
 pod 'MBProgressHUD',      '1.1.0'
 pod 'SSZipArchive',       '2.1.3'
 pod 'MMKV',               '1.0.12'
+pod 'SDCycleScrollView',  '1.75'
 
 end
