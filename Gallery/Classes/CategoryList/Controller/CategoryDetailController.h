e25a3a4c683a3c7f3703c31e47b1a2dff8be0b97
andong <dongan708@gmail.com>
2018-11-08 18:50:41 +0800

    updates

---
+++ Gallery/Classes/CategoryList/Controller/CategoryDetailController.h
@@ -0,0 +1,16 @@
+//
+//  CategoryDetallController.h
+//  Gallery
+//
+//  Created by admin on 2018/11/8.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryDetailController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Gallery/Classes/CategoryList/View/CategoryDetallCell.h
@@ -0,0 +1,17 @@
+//
+//  CategoryDetallCell.h
+//  Gallery
+//
+//  Created by admin on 2018/11/8.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryDetallCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
