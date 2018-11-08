b282ca4eec93cfb60dcd383ee3fb74388f3a9d77
SunChengLei <645716221@qq.com>
2018-11-08 18:36:16 +0800

    update

---
+++ Gallery/Classes/CategoryList/Controller/CategoryDetailController.m
@@ -0,0 +1,32 @@
+//
+//  CategoryDetallController.m
+//  Gallery
+//
+//  Created by admin on 2018/11/8.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import "CategoryDetailController.h"
+
+@interface CategoryDetailController ()
+
+@end
+
+@implementation CategoryDetailController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Gallery/Classes/CategoryList/View/CategoryDetallCell.m
@@ -0,0 +1,13 @@
+//
+//  CategoryDetallCell.m
+//  Gallery
+//
+//  Created by admin on 2018/11/8.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import "CategoryDetallCell.h"
+
+@implementation CategoryDetallCell
+
+@end
