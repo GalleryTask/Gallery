863e7996f0ac3d30e3bba9d0e8e2ba16dc334b73
SunChengLei <645716221@qq.com>
2018-11-08 15:51:46 +0800

    update

---
+++ Gallery/Classes/CategoryList/Controller/CategoryListCollectionViewCell.m
@@ -0,0 +1,70 @@
+//
+//  CategoryListCollectionViewCell.m
+//  Gallery
+//
+//  Created by admin on 2018/11/8.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import "CategoryListCollectionViewCell.h"
+
+@interface CategoryListCollectionViewCell ()
+
+
+@property(nonatomic, strong) UIImageView *pictureImageView;
+@property(nonatomic, strong) UILabel     *titleLabel;
+
+@end
+
+@implementation CategoryListCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+-(void)layoutSubviews{
+    [super layoutSubviews];
+
+    [self.pictureImageView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(SCALE_SIZE*5);
+        make.width.mas_equalTo((SCREEN_WIDTH*0.8-(SCALE_SIZE*10))/2-SCALE_SIZE*10);
+        make.top.mas_equalTo(SCALE_SIZE *15);
+        make.height.mas_equalTo(((SCREEN_WIDTH*0.8-(SCALE_SIZE*10))/2-SCALE_SIZE*10) /124 *100);
+    }];
+    
+    [self.titleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+       
+        make.left.right.equalTo(self.pictureImageView);
+//        make.left.mas_equalTo(SCALE_SIZE*5);
+//        make.right.mas_equalTo(-SCALE_SIZE*5);
+        make.top.mas_equalTo(self.pictureImageView.mas_bottom).offset(SCALE_SIZE*10);
+     
+    }];
+}
+
+-(UIImageView *)pictureImageView{
+    if (!_pictureImageView) {
+        _pictureImageView = [[UIImageView alloc] init];
+        [_pictureImageView setContentMode:UIViewContentModeScaleAspectFit];
+//        [_arrowImgView setImage:[UIImage imageNamed:@"arrow"]];
+        _pictureImageView.backgroundColor = [UIColor grayColor];
+        [self.contentView addSubview:_pictureImageView];
+    }
+    return _pictureImageView;
+}
+
+-(UILabel *)titleLabel{
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] init];
+        [_titleLabel setTextColor:[UIColor blackColor]];
+        [_titleLabel setFont:FONTSIZE(13)];
+       // [_titleLabel setTextAlignment:(NSTextAlignmentCenter)];
+        [_titleLabel setText:@"精品彩盒方案一精品彩盒方案一"];
+        [self.contentView addSubview:_titleLabel];
+    }
+    return _titleLabel;
+}
+
+
+@end
--- Gallery/Classes/CategoryList/Controller/ProductsViewController.m
+++ Gallery/Classes/CategoryList/Controller/ProductsViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "ProductsViewController.h"
+#import "CategoryListCollectionViewCell.h"
+@interface ProductsViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
 
-@interface ProductsViewController ()
+@property(nonatomic, strong)UICollectionView *listCollection;
 
 @end
-
+static NSString *cellIdentList = @"listCell";
 @implementation ProductsViewController
 
 - (void)viewDidLoad {
@@ -19,7 +21,48 @@
   [self.view setFrame:CGRectMake(SCREEN_WIDTH*0.2, 0, SCREEN_WIDTH*0.8, SCREEN_HEIGHT)];
 //  [self.view setBackgroundColor:BASECOLOR_LIGHTGRAY];
 
+    [self.listCollection setContentInset:UIEdgeInsetsMake(0, 0, SafeAreaBottomHeight+BASE_HEIGHT, 0)];
 }
+
+#pragma maek -UICollectionViewDelegateFlowLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    //return CGSizeMake((SCREEN_WIDTH*0.8-(SCALE_SIZE*10))/2, (SCREEN_WIDTH*0.8-(SCALE_SIZE*10))/2+ SCALE_SIZE*30);
+    return CGSizeMake((SCREEN_WIDTH*0.8-(SCALE_SIZE*10))/2, ((SCREEN_WIDTH*0.8-(SCALE_SIZE*10))/2-SCALE_SIZE*10) /124 *100 + SCALE_SIZE * 40);
+}
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
+    return UIEdgeInsetsMake(0,SCALE_SIZE*5,0,SCALE_SIZE*5);
+}
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section{
+    
+    return 0;
+}
+//列
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section{
+    return 0;
+    
+}
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 7;
+}
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CategoryListCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentList forIndexPath:indexPath];
+  
+    if (indexPath.row == 0) {
+        cell.contentView.backgroundColor = [UIColor redColor];
+    }
+    return cell;
+}
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+  
+    
+}
+
+
 
 #pragma mark - 一级tableView滚动时 实现当前类tableView的联动
 - (void)scrollToSelectedIndexPath:(NSIndexPath *)indexPath {
@@ -28,6 +71,33 @@
 //                              animated:YES
 //                        scrollPosition:UITableViewScrollPositionTop];
 }
+
+-(UICollectionView *)listCollection
+{
+    if (!_listCollection) {
+        UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+        //设置collectionView滚动方向
+        //    [layout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+        //该方法也可以设置itemSize
+        //layout.itemSize =CGSizeMake(110, 150);
+        
+        //2.初始化collectionView
+        _listCollection = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH*0.8, SCREEN_HEIGHT) collectionViewLayout:layout];
+        
+        _listCollection.backgroundColor = [UIColor whiteColor];
+        //注意，此处的ReuseIdentifier 必须和 cellForItemAtIndexPath 方法中 一致 均为 cellId
+        [_listCollection registerClass:[CategoryListCollectionViewCell class] forCellWithReuseIdentifier:cellIdentList];
+        //4.设置代理
+        _listCollection.delegate = self;
+        _listCollection.dataSource = self;
+        
+        [self.view addSubview:_listCollection];
+        
+    }
+    return _listCollection;
+}
+
+
 
 /*
 #pragma mark - Navigation
