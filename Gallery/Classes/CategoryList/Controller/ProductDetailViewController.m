4194f050841096e65b529a9f804848a1697252ae
SunChengLei <645716221@qq.com>
2018-11-09 17:43:42 +0800

    update

--- Gallery/Classes/CategoryList/Controller/ProductDetailViewController.m
+++ Gallery/Classes/CategoryList/Controller/ProductDetailViewController.m
@@ -17,6 +17,8 @@
 @property (nonatomic, strong) UIButton *exhibitionBtn;
 @property (nonatomic, strong) SDCycleScrollView  *cycleScrollView; // 轮播图
 @property (nonatomic, strong) HomeShowView  *showView;
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *detailLabel;
 
 @end
 
@@ -28,6 +30,19 @@
   [self.view addSubview:self.exhibitionBtn];
   [self.view addSubview:self.scrollView];
   self.cycleScrollView.imageURLStringsGroup = @[@"",@""];
+  
+  [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//    make.left.equalTo(self.showView);
+    make.left.mas_equalTo(SCALE_SIZE * 15);
+    make.right.mas_equalTo(-SCALE_SIZE* 15);
+    make.top.equalTo(self.showView.mas_bottom).offset(SCALE_SIZE*10);
+  }];
+  [self.detailLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+    make.left.right.equalTo(self.titleLabel);
+    make.top.equalTo(self.titleLabel.mas_bottom).offset(SCALE_SIZE*5);
+  }];
+
+
 
 }
 
@@ -65,11 +80,35 @@
   return _showView;
 }
 
+-(UILabel *)titleLabel {
+  if (!_titleLabel) {
+    _titleLabel  = [[UILabel alloc] init];
+    [_titleLabel setFont:[UIFont systemFontOfSize:SCALE_SIZE*16 weight:UIFontWeightBold]];
+    [_titleLabel setTextColor:BASECOLOR_BLACK_333];
+    [_titleLabel setText:@"与生俱来的艺术品，包装本该不同"];
+    [_scrollView addSubview:_titleLabel];
+  }
+  return _titleLabel;
+}
+
+-(UILabel *)detailLabel{
+  if (!_detailLabel) {
+    _detailLabel  = [[UILabel alloc] init];
+    [_detailLabel setFont:FONTSIZE(14)];
+    [_detailLabel setTextColor:BASECOLOR_BLACK_999];
+    [_detailLabel setNumberOfLines:1];
+    //[_detailLabel setText:@"我们从来不怀疑，“有趣”可能给产品带来巨大的价值。我们试图用略带夸张与戏虐的人物表情来呈现这三个与苹果有关联的人物。引发用户快速的认知和趣味的联系"];
+    [_showView addSubview:_detailLabel];
+  }
+  return _detailLabel;
+}
+
 -(UIScrollView *)scrollView {
   if (!_scrollView) {
     _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - NAVIGATIONBAR_HEIGHT -SafeAreaBottomHeight - 50)];
     AdjustsScrollViewInsetNever(self, _scrollView);
     [_scrollView setShowsVerticalScrollIndicator:NO];
+     [_scrollView setShowsHorizontalScrollIndicator:NO];
   }
   return _scrollView;
 }
