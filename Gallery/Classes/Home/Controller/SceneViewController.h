b0d69715d0e6024122fbee3053ce4967dc636a73
andong <dongan708@gmail.com>
2018-11-09 17:12:00 +0800

    updates

---
+++ Gallery/Classes/Home/Controller/SceneViewController.h
@@ -0,0 +1,17 @@
+//
+//  SceneViewController.h
+//  Gallery
+//
+//  Created by 安东 on 09/11/2018.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SceneViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
