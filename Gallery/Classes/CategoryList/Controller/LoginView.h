b111d95c1c7d5e3d916b5080cb9e3ed13f716170
SunChengLei <645716221@qq.com>
2018-11-14 14:38:02 +0800

    update

--- Gallery/Classes/CategoryList/Controller/LoginView.h
+++ Gallery/Classes/CategoryList/Controller/LoginView.h
@@ -9,10 +9,20 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@protocol LoginViewDelegate <NSObject>
 
+- (void)loginViewloginBtnClick;//登录的代理
+- (void)loignViewCountryBtnClick;//国家代码的代理
+- (void)loginViewServiceBtnClick;//用户协议代理
+- (void)loginViewSendCodeClick;//验证码代理
+- (void)loginViewForgetPasswordClick;//忘记密码
+- (void)loginViewRegisterClick;//注册代理
+@end
 @interface LoginView : UIView
 @property (strong, nonatomic) UITextField        *accountField;       // 账号
 @property (strong, nonatomic) UITextField        *passwordField;      // 密码
+
+@property (weak,   nonatomic) id <LoginViewDelegate> delegate;
 
 @end
 
