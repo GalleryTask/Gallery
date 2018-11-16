5c351c927754809ae006666130000ff10e9d7aff
andong <dongan708@gmail.com>
2018-11-16 18:57:17 +0800

    updates

---
+++ Gallery/Classes/Order/Controller/OrderSubmitController.h
@@ -0,0 +1,17 @@
+//
+//  OrderSubmitController.h
+//  Gallery
+//
+//  Created by 安东 on 16/11/2018.
+//  Copyright © 2018 安东. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OrderSubmitController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Gallery/Classes/Order/Controller/OrderSubmitController.m
+++ Gallery/Classes/Order/Controller/OrderSubmitController.m
@@ -1,35 +1,36 @@
 //
-//  OrderPreviewController.m
+//  OrderSubmitController.m
 //  Gallery
 //
 //  Created by 安东 on 16/11/2018.
 //  Copyright © 2018 安东. All rights reserved.
 //
 
-#import "OrderPreviewController.h"
+#import "OrderSubmitController.h"
 #import "SchemeCell.h"
 #import "PreviewCell.h"
 #import "DeliveryDateCell.h"
 #import "SelectCountCell.h"
 #import "BottomSelectBar.h"
-#import "OrderSubmitController.h"
 
-@interface OrderPreviewController () <BottomSelectBarDelegate>
+@interface OrderSubmitController () <BottomSelectBarDelegate>
 
-@property (nonatomic, strong) UIView  *footerView;
 @property (nonatomic, strong) BottomSelectBar  *bottomBar;
+@property (nonatomic, strong) UIView  *headerView;
 
 @end
 
-@implementation OrderPreviewController
+@implementation OrderSubmitController
 
 -(void)viewDidLoad {
   [super viewDidLoad];
-  self.navigationItem.title = @"清单明细";
+  
+  self.navigationItem.title = @"订单详情";
+  
   [self.tableView registerClass:[SchemeCell class] forCellReuseIdentifier:@"SchemeCell"];
   [self.tableView registerClass:[PreviewCell class] forCellReuseIdentifier:@"PreviewCell"];
   [self.tableView registerClass:[DeliveryDateCell class] forCellReuseIdentifier:@"DeliveryDateCell"];
-   [self.tableView registerClass:[SelectCountCell class] forCellReuseIdentifier:@"SelectCountCell"];
+  [self.tableView registerClass:[SelectCountCell class] forCellReuseIdentifier:@"SelectCountCell"];
   self.dataSource = [NSMutableArray arrayWithObjects:
                      @{@"title":@"EPE",@"detail":@"结构一 / 规格18g/L",@"count":@"x 1个"},
                      @{@"title":@"标签",@"detail":@"A款标签/铜版纸",@"count":@"x 1个"},
@@ -37,21 +38,9 @@
                      @{@"title":@"平面设计",@"detail":@"主位设计图,副位设计图",@"count":@""},
                      @{@"title":@"印刷工艺",@"detail":@"胶印亚膜",@"count":@""}, nil];
   self.tableView.contentInset = UIEdgeInsetsMake(0, 0, SCALE_SIZE*50, 0);
-  [self.tableView setTableFooterView:self.footerView];
   
-  UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCALE_SIZE*10)];
-  [headerView setBackgroundColor:BASECOLOR_BACKGROUND_GRAY];
-  [self.tableView setTableHeaderView:headerView];
+  [self.tableView setTableHeaderView:self.headerView];
 }
-
--(void)viewDidAppear:(BOOL)animated {
-  [super viewDidAppear:animated];
-  
-  BaseNavigationController *nav = (BaseNavigationController *)self.navigationController;
-  [nav setNavigationBarRightItemWithButtonTitle:@"保存"];
-  [nav setNavigationBarRightItemWithImageName:@"" highlightImageName:@""];
-}
-
 
 #pragma mark tableview delegate
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -62,9 +51,6 @@
   } else if (indexPath.row == 6) {
     DeliveryDateCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DeliveryDateCell" forIndexPath:indexPath];
     return cell;
-  } else if (indexPath.row == 7) {
-    SelectCountCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SelectCountCell" forIndexPath:indexPath];
-    return cell;
   }
   
   PreviewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PreviewCell" forIndexPath:indexPath];
@@ -74,13 +60,13 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
   
-  return 8;
+  return 7;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
   if (indexPath.row == 0) {
     return SCALE_SIZE*186;
-  } else if (indexPath.row == 6 || indexPath.row == 7) {
+  } else if (indexPath.row == 6) {
     return SCALE_SIZE*52;
   }
   else {
@@ -88,52 +74,31 @@
   }
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-  
-  [tableView deselectRowAtIndexPath:indexPath animated:YES];
-}
-
-
 #pragma marks - BottomSelectBar delegate
 -(void)bottomBarWithLeftBtnClick:(id)sender {
   
 }
 
 -(void)bottomBarWithRightBtnClick:(id)sender {
-  OrderSubmitController *vc = [[OrderSubmitController alloc] init];
-  [self.navigationController pushViewController:vc animated:YES];
-}
-
-#pragma marks - getters
--(UIView *)footerView {
-  if (!_footerView) {
-    _footerView = [[UIView alloc] init];
-    [_footerView setFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCALE_SIZE*64)];
-    
-    UIView *lineView = [[UIView alloc] init];
-    [lineView setFrame:CGRectMake(SCALE_SIZE*10, 0, SCREEN_WIDTH-SCALE_SIZE*20, 0.5)];
-    [lineView setBackgroundColor:BASECOLOR_LINE];
-    [_footerView addSubview:lineView];
-    
-    UILabel *label = [[UILabel alloc] init];
-    [label setFrame:CGRectMake(0, 0.5, SCREEN_WIDTH-SCALE_SIZE*10, SCALE_SIZE*63)];
-    [label setTextColor:BASECOLOR_BLACK_333];
-    [label setFont:FONTSIZE(14)];
-    [label setTextAlignment:NSTextAlignmentRight];
-    [label setText:@"总金额：¥ 30.00"];
-    [_footerView addSubview:label];
-  }
-  return _footerView;
+  
 }
 
 -(BottomSelectBar *)bottomBar {
   if (!_bottomBar) {
-    _bottomBar = [[BottomSelectBar alloc] initWithLeftTitle:@"加入订货单" rightTitle:@"提交打样" delegate:self];
+    _bottomBar = [[BottomSelectBar alloc] initWithLeftTitle:@"总金额：￥30.00" rightTitle:@"付款" delegate:self];
     [self.tableView addSubview:_bottomBar];
   }
   return _bottomBar;
 }
 
+-(UIView *)headerView {
+  if (!_headerView) {
+    _headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCALE_SIZE*102)];
+    [_headerView setBackgroundColor:BASECOLOR_BACKGROUND_GRAY];
+    
+  }
+  return _headerView;
+}
 
 
 -(void)viewDidLayoutSubviews {
@@ -144,5 +109,4 @@
     make.height.mas_equalTo(SCALE_SIZE*50);
   }];
 }
-
 @end
